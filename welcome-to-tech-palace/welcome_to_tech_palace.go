8d544119bc79d1a7c89ba19e211d94ab855e5d27
Conrad <conradjohnsoniii@pm.me>
2022-01-03 12:54:53 -0500

    add: strings.Repeat / Join / Writer functions

--- welcome-to-tech-palace/welcome_to_tech_palace.go
+++ welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -4,6 +4,12 @@ import (
 	"strings"
 )
 
+const (
+	WelcomeMsg        = "Welcome to the Tech Palace, "
+	WelcomeBorderChar = "*"
+	Newline           = "\n"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
 	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
@@ -17,6 +23,32 @@ func AddBorder(welcomeMsg string, numStarsPerLine int) string {
 	}
 	displayMessage := loyalityGreeting + "\n" + welcomeMsg + "\n" + loyalityGreeting
 	return displayMessage
+}
+
+// Using strings.Repeat
+func AddBorderByRepeat(welcomeMsg string, numStarsPerLine int) string {
+	return strings.Repeat("*", numStarsPerLine) + "\n" + welcomeMsg + "\n" + strings.Repeat("*", numStarsPerLine)
+}
+
+// Using strings.Join
+func AddBorderByArray(welcomeMsg string, numStarsPerLine int) string {
+	var welcomeBorder string
+	for i := 0; i < numStarsPerLine; i++ {
+		welcomeBorder = strings.Join([]string{welcomeBorder, WelcomeBorderChar}, "")
+	}
+	message := []string{welcomeBorder + Newline, welcomeMsg + Newline, welcomeBorder}
+	return strings.Join(message, "")
+}
+
+// Using strings.Builder
+func AddBorderByBuilder(welcomeMsg string, numStarsPerLine int) string {
+	var sb strings.Builder
+	var welcomeBorder string
+	for i := 0; i < numStarsPerLine; i++ {
+		welcomeBorder = strings.Join([]string{welcomeBorder, WelcomeBorderChar}, "")
+	}
+	sb.WriteString(welcomeBorder + Newline + welcomeMsg + Newline + welcomeBorder)
+	return sb.String()
 }
 
 // CleanupMessage cleans up an old marketing message.
